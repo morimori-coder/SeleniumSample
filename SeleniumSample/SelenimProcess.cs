d916b841b43817433de1dbd925ca914c81f26155
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-07-23 10:56:51 +0900

    プロジェクト ファイルを追加します。

---
+++ SeleniumSample/SelenimProcess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumSample
+{
+    internal class SelenimProcess
+    {
+        private IWebDriver _WebDriver;
+        public void DoSelenium()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+
+            using (_WebDriver = new ChromeDriver(service, options)) 
+            {
+                _WebDriver.Navigate().GoToUrl("https://www.google.co.jp");
+            }
+        }
+    }
+}
