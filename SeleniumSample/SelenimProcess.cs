518fbd5863158933bad00da11ad93226824123f5
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-07-25 07:33:32 +0900

    Chromeで特定のキーワードを検索して、結果をすべて取得する

--- SeleniumSample/SelenimProcess.cs
+++ SeleniumSample/SelenimProcess.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -11,7 +9,10 @@ namespace SeleniumSample
     internal class SelenimProcess
     {
         private IWebDriver _WebDriver;
-        public void DoSelenium()
+
+        public List<IWebElement> SearchResult { get; set; }
+
+        public void SearchChrome()
         {
             ChromeDriverService service = ChromeDriverService.CreateDefaultService();
             service.HideCommandPromptWindow = true;
@@ -22,6 +23,27 @@ namespace SeleniumSample
             using (_WebDriver = new ChromeDriver(service, options)) 
             {
                 _WebDriver.Navigate().GoToUrl("https://www.google.co.jp");
+                var element = _WebDriver.FindElement(By.CssSelector("検索バー"));
+                element.SendKeys("検索したいキーワード");
+                element.SendKeys(Keys.Enter);
+
+                var h3List = _WebDriver.FindElements(By.ClassName("各検索結果")).ToList();
+                var nextLink = _WebDriver.FindElement(By.Id("つぎへ"));
+                int counter = 0;
+                try 
+                {
+                    while (nextLink != null)
+                    {
+                        nextLink.Click();
+                        h3List.AddRange(_WebDriver.FindElements(By.ClassName("各検索結果")).ToList());
+                        nextLink = _WebDriver.FindElement(By.Id("つぎへ"));
+                        counter++;
+                    }
+                    this.SearchResult = h3List;
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
     }
